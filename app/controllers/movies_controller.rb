2103eaf69c2866ce58437eb90ca0864b03741478
anupamroy8 <anupamroy8@gmail.com>
2024-05-24 23:19:31 +0530

    add validations, form partials, more fields to movies table

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
+require 'byebug'
+
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.released
   end
 
   def show
@@ -23,16 +25,25 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to movie_path(@movie)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render 'new'
+    end
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
 
   private
 
   def movie_params
     params.require(:movie).
-      permit(:title, :description, :rating, :released_on, :total_gross)
+      permit(:title, :description, :rating, :released_on, :total_gross,
+      :director, :duration, :image_file_name)
   end
 
 end
